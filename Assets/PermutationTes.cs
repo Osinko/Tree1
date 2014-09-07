404c0739708d9b404e63489c80113abed6814f8a
Osinko <yoshinari_m@msn.com>
2014-09-07 17:09:59 +0900

    no message

--- Assets/PermutationTes.cs
+++ Assets/PermutationTes.cs
@@ -7,20 +7,25 @@ public class PermutationTes : MonoBehaviour
 {
 		void Start ()
 		{
-				var num = Enumerable.Range (1, 5).Select (x => x).ToArray ();
-				Method (0, "", num, 3);
+				PermutationNumber (6, 3);
 		}
 
-		void Method (int columns, string resume, int[] num, int r)
+		void PermutationNumber (int n, int r)
+		{
+				var number = Enumerable.Range (1, n).Select (x => x).ToArray ();
+				PermutationNest (number, r, 0, "");
+		}
+
+		void PermutationNest (int[] n, int r, int columns, string resume)
 		{
 				if (columns < r) {
 						columns++;
 						//forループの中で親の変数を書き換えていないか常に注意する事
-						for (int i = 0; i < num.Length; i++) {
+						for (int i = 0; i < n.Length; i++) {
 								string resumeClone = resume;		//forループの中で親が変更されないように新しく変数を作る（重要）
-								resumeClone += num [i].ToString ();
-								int[] numClone = num.Where (x => x != num [i]).ToArray ();	//これも作る
-								Method (columns, resumeClone, numClone, r);
+								resumeClone += n [i].ToString () + ",";
+								int[] numClone = n.Where (x => x != n [i]).ToArray ();	//これも作る
+								PermutationNest (numClone, r, columns, resumeClone);
 						}
 				} else {
 						print (resume);
