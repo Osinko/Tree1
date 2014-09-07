30ba9a472417729559cbb36fb1b54be8436742ed
Osinko <yoshinari_m@msn.com>
2014-09-07 16:52:36 +0900

    no message

--- Assets/PermutationTes.cs
+++ Assets/PermutationTes.cs
@@ -8,21 +8,21 @@ public class PermutationTes : MonoBehaviour
 		void Start ()
 		{
 				var num = Enumerable.Range (1, 5).Select (x => x).ToArray ();
-				//string resume = "0";
 				Method (0, "", num);
 		}
 
 		void Method (int columns, string resume, int[] num)
 		{
-				if (columns <= 2) {
+				if (columns < 2) {
 						columns++;
 						for (int i = 0; i < num.Length; i++) {
 								string resumeClone = resume;
 								resumeClone += num [i].ToString ();
 								int[] numClone = num.Where (x => x != num [i]).ToArray ();
-								//print ("");
 								Method (columns, resumeClone, numClone);
 						}
+				} else {
+						print (resume);
 				}
 		}
 }
